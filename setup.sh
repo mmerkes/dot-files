c788461d94974a52324cfcdbcecb01f264ebfe6f
Merkes <matt.merkes@gmail.com>
2017-03-19 18:00:01 -0700

    Fixes .tmux.conf move

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 # Move the dot files to the home directory
 cp .bash_profile $HOME/.bash_profile
 cp .bashrc $HOME/.bashrc
-cp .tmux.conf $HOME/tmux.conf
+cp .tmux.conf $HOME/.tmux.conf
 cp .vimrc $HOME/.vimrc
